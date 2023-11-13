18f32ec3d6b3d41b2b8f4de244a1540fb0d51fa4
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2023-11-13 00:28:59 -0600

    Setup inicial del proyecto con avance de la pagina de login

---
+++ src/pages/login/LoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="h-screen w-screen flex items-center justify-center">
+        <div class="bg-[#2B59C3] w-[50%] h-[50%] rounded-xl">
+            <form action="" class="flex w-[80%] h-full flex-col">
+                <div class="flex flex-col">
+                    <label for="">Usuario</label>
+                    <input type="text">
+                </div>
+                <div class="flex flex-col">
+                    <label for="">Contraseña</label>
+                    <input type="text">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('../pages/login/LoginPage.vue')
+    },
+  ]
+})
+
+export default router
